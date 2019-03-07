8108c7a4bee824a2eabed8cbf143d8b8d554d9d5
Wallace <wallace.balaniuc@gmail.com>
2019-03-06 21:29:48 -0500

    Scrolling Background

---
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float scrollSpeed = 0.05f;
+
+    void Start()
+    {
+        InvokeRepeating("MoveBG", 0.5f, 0.5f);
+    }
+
+    void MoveBG()
+    {
+        GetComponent<Renderer>().material.mainTextureOffset =
+            new Vector2(GetComponent<Renderer>().material.mainTextureOffset.x + scrollSpeed, 0);
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0.0f);
+    }
+    */
+}
