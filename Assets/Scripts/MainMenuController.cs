6944dbdc362d87cd10f7860e2ff298aebcb29d1f
Wallace <wallace.balaniuc@gmail.com>
2019-03-06 20:30:48 -0500

    Added Main Menu and Controls

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // What happens when I click the start button?
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // What happens when I click the quit button?
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
