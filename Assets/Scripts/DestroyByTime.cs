f7c0d1cf9fa543dc7659968ff370531daff0497a
Wallace <wallace.balaniuc@gmail.com>
2019-03-13 20:30:03 -0400

    Added cherry pickups

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float timeToDestroy = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, timeToDestroy);
+    }
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int cherryCounter = 0;
+    public GameObject itemFeedbackPref;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if 'other' is a tagged an item
+        if(other.CompareTag("Item"))
+        {
+            // Pick up the item
+            cherryCounter++;
+            // Instantiate the item feedback object
+            Instantiate(itemFeedbackPref, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+        }
+    }
+}
