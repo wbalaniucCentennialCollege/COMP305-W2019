4c41a5b97d39a91b617665c5da0c6167701c8948
Wallace <wallace.balaniuc@gmail.com>
2019-03-20 21:11:52 -0400

    Added end level panel

---
+++ Assets/Scripts/LevelEndTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndTrigger : MonoBehaviour
+{
+    public GameObject ui_endLevelPanel;
+
+    // Reserved by Unity to detect when an object enters a collider
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            // Show my UI panel
+            ui_endLevelPanel.SetActive(true);
+        }
+    }
+}
