b82e1c80da878518e73b1f7a603b1033e66c760f
Wallace <wallace.balaniuc@gmail.com>
2019-03-27 19:47:30 -0400

    Initialized item counter UI

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public UI_CherryCounter ui_cherryCounter;
+
+    private int itemCounter = 0;    // How many items I have picked up
+    private int totalItemCount = 0; // How many items are in my level
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GameObject.Find("UI_CherryCounter").GetComponent<UI_CherryCounter>();
+        totalItemCount = GameObject.FindGameObjectsWithTag("Item").Length;
+        ui_cherryCounter.UpdateUI(itemCounter, totalItemCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/UI_CherryCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CherryCounter : MonoBehaviour
+{
+    private Text cherryText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cherryText = this.GetComponent<Text>();
+    }
+
+    public void UpdateUI(int qty, int maxItemInLevel)
+    {
+        cherryText.text = qty + " / " + maxItemInLevel;
+    }
+}
