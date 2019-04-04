91a439b83da06c29f935f82fdd62c188f157b782
Wallace <wallace.balaniuc@gmail.com>
2019-04-03 20:16:49 -0400

    Added audio mixer and snapshots

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class GameController : MonoBehaviour
 {
+    [Header("User Interface")]
     public UI_CherryCounter ui_cherryCounter;
     public GameObject ui_EndLevelPanel;
+
+    [Header("Audio")]
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpaused;
 
     private int itemCounter = 0;    // How many items I have picked up
     private int totalItemCount = 0; // How many items are in my level
@@ -17,6 +23,25 @@ public class GameController : MonoBehaviour
         // GameObject.Find("UI_CherryCounter").GetComponent<UI_CherryCounter>();
         totalItemCount = GameObject.FindGameObjectsWithTag("Item").Length;
         ui_cherryCounter.UpdateUI(itemCounter, totalItemCount);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)) // Pause or unpause
+        {
+            // Set my audio snapshot to the pause or unpause snapshot
+            // Pause my game.
+            Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+            
+            if(Time.timeScale == 0) // Paused
+            {
+                paused.TransitionTo(0.01f);
+            }
+            else // Unpaused
+            {
+                unpaused.TransitionTo(0.01f);
+            }
+        }
     }
 
     // Called when an item is picked up
